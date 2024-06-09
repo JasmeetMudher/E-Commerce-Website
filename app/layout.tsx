d071594ac01ed32fa1017ed5abd1e1fbcb5b085a
JasmeetMudher <jsmudher4@gmail.com>
2024-06-09 11:44:37 +0300

    Create-Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from '@/components/header/header'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-h-screen flex flex-col">
+          <Header  />
+          {children}  
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                  <p>
+                    Copyright © 2023 - All right reserved by Next Amazona V2
+                  </p>
+                </footer>
+        </div>
+        </body>
     </html>
   );
 }
