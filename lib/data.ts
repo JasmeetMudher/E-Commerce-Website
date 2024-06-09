564ccea17d47a79ddd0328478113c3d4f8a64714
JasmeetMudher <jsmudher4@gmail.com>
2024-06-09 12:24:33 +0300

    Home-Page-Completed

---
+++ lib/data.ts
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+          name: 'Handmade Beaded Necklace',
+          slug: 'handmade-beaded-necklace',
+          category: 'Accessories',
+          image: '/images/beaded_necklace1.jpg',
+          price: 50,
+          brand: 'Artisan Creations',
+          rating: 4.8,
+          numReviews: 15,
+          countInStock: 30,
+          description: 'A beautifully handcrafted beaded necklace',
+          isFeatured: true,
+          banner: '/images/beaded_necklace_banner1.jpg'
+        },
+        {
+          name: 'Handmade Woven Basket',
+          slug: 'handmade-woven-basket',
+          category: 'Home Decor',
+          image: '/images/woven_basket1.jpg',
+          price: 40,
+          brand: 'Crafted Traditions',
+          rating: 4.7,
+          numReviews: 12,
+          countInStock: 25,
+          description: 'A durable and stylish handwoven basket',
+          isFeatured: false,
+          banner: '/images/woven_basket_banner1.jpg'
+        },
+        {
+          name: 'Handmade Ceramic Mug',
+          slug: 'handmade-ceramic-mug',
+          category: 'Kitchenware',
+          image: '/images/ceramic_mug1.jpg',
+          price: 25,
+          brand: 'Artisan Pottery',
+          rating: 4.9,
+          numReviews: 20,
+          countInStock: 50,
+          description: 'A unique and beautifully crafted ceramic mug',
+          isFeatured: true,
+          banner: '/images/ceramic_mug_banner1.jpg'
+        },
+        {
+          name: 'Handmade Wooden Spoon',
+          slug: 'handmade-wooden-spoon',
+          category: 'Kitchenware',
+          image: '/images/wooden_spoon1.jpg',
+          price: 15,
+          brand: 'Crafted Traditions',
+          rating: 4.6,
+          numReviews: 18,
+          countInStock: 40,
+          description: 'A sturdy and beautifully crafted wooden spoon',
+          isFeatured: false,
+          banner: '/images/wooden_spoon_banner1.jpg'
+        },
+        {
+          name: 'Handmade Leather Wallet',
+          slug: 'handmade-leather-wallet',
+          category: 'Accessories',
+          image: '/images/leather_wallet1.jpg',
+          price: 60,
+          brand: 'Artisan Leatherworks',
+          rating: 4.7,
+          numReviews: 22,
+          countInStock: 15,
+          description: 'A stylish and durable handmade leather wallet',
+          isFeatured: true,
+          banner: '/images/leather_wallet_banner1.jpg'
+        },
+        
+      ]
+}
+
+export default data;
